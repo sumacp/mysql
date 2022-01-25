d379f11bbdb07c339b80dca3cd367ec555596830
sumacp <you@sumacp1234@gmail.com>
2022-01-25 14:18:54 +0530

    first commit

---
+++ src/main/java/net/codejava/productREST/ProductRepository.java
@@ -0,0 +1,8 @@
+package net.codejava.productREST;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+
+}
